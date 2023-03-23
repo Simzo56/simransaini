ba98b6af2f50f82321d49c5931576f7d68287338
Simzo56 <117115431+Simzo56@users.noreply.github.com>
2023-03-23 20:51:27 +0530

    Add files via upload

---
+++ DNF.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class DNF {
+    public static void main(String[]args){
+        int arr[]={0,1,1,1,0,0,2,2,1,0,1,0,2,2};
+        int n=arr.length;
+        int i;
+        Arrays.sort(arr);
+        for(i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+        
+            
+            }
+        }
+    
+
+    
+    
+
