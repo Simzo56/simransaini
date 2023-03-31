9cb5a6c5613ef0a4ab4565e3f256c63a6e194879
Simzo56 <117115431+Simzo56@users.noreply.github.com>
2023-03-31 16:17:58 +0530

    Add files via upload

---
+++ average.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class average {
+    
+    public static void avg(int a,int b,int c){
+         int calculate=(a+b+c)/3;
+         System.out.println(calculate);
+         return;
+    }
+    
+
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        avg(a, b, c);
+    }
+    
+}
