5e098534d748289681c58f497599349eccd32bc7
Simzo56 <117115431+Simzo56@users.noreply.github.com>
2023-05-07 14:30:08 +0530

    Add files via upload

---
+++ R10.java
@@ -0,0 +1,40 @@
+public class R10 {
+    static void Space(int numberofSpace){
+        if(numberofSpace==0){
+            return;
+        }
+        System.out.print(" ");
+        Space(numberofSpace-1);
+
+    }
+    static void number(int number){
+        if(number==0){
+            return;
+        }
+        System.out.print(number);
+        number(number-1);
+        
+
+    }
+    static void Line(int numberofLines,int totalnumberofLines){
+        if(numberofLines==0){
+            return;
+        }
+        Line(numberofLines-1,totalnumberofLines);
+        Space(totalnumberofLines-numberofLines);
+        number(numberofLines);
+        System.out.println();
+    }
+    static void pattern(int numberofLines){
+        Line(numberofLines,numberofLines);
+    }
+    public static void main(String[]args){
+        pattern(8);
+    }
+    
+}
+
+
+
+
+
